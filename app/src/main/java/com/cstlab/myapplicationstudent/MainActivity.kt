0f232ec3ae6cec0dafa9218950a136decb995981
Thinleydema7 <159892860+Thinleydema7@users.noreply.github.com>
2026-08-04 16:04:38 +0600

    trial for now

--- app/src/main/java/com/cstlab/myapplicationstudent/MainActivity.kt
+++ app/src/main/java/com/cstlab/myapplicationstudent/MainActivity.kt
@@ -33,7 +33,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "yo yo  $name!",
+        text = "yoyo $name!",
         modifier = modifier
     )
 }
